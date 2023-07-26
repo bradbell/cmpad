7c8df36b9f1f1b5894faaf784fc58da5082380c8
Brad Bell <bradbell@seanet.com>
2023-07-26 13:39:51 -0700

    main: add get_package.sh sacado.

--- bin/get_package.sh
+++ bin/get_package.sh
@@ -21,7 +21,7 @@ fi
 if [ "$#" != 1 ]
 then
    echo 'usage: bin/get_package package'
-   echo 'where package is adolc, or cppad.'
+   echo 'where package is adolc, cppad, sacado.'
    exit 1
 fi
 #
@@ -55,6 +55,16 @@ case $package in
    configure="$configure -D cppad_cxx_flags='-D CPPAD_DEBUG_AND_RELEASE'"
    ;;
 
+   sacado)
+   web_page='https://github.com/trilinos/Trilinos/archive/refs/tags'
+   version='trilinos-release-14-0-0'
+   configure='cmake -S .. -B . -D CMAKE_BUILD_TYPE=release'
+   configure="$configure -D Trilinos_ENABLE_Sacado=ON"
+   configure="$configure -D Sacado_ENABLE_TESTS=OFF"
+   configure="$configure -D CMAKE_INSTALL_PREFIX=$prefix"
+   # -D Trilinos_INSTALL_LIB_DIR=$prefix/$libdir
+   ;;
+
    *)
    echo 'bin/get_package.sh: package'
    echo 'package is not adolc, or cppad'
@@ -70,6 +80,14 @@ else
    n_job=$(sysctl -n hw.ncpu)
 fi
 #
+# package_top_srcdir
+if echo $web_page | grep '\.git#' > /dev/null
+then
+   package_top_srcdir="$package.git"
+else
+   package_top_srcdir="$package"
+fi  
+#
 # configured_flag
 configured_flag="external/$package.configured"
 # -----------------------------------------------------------------------------
@@ -77,7 +95,7 @@ if [ -e "$configured_flag" ]
 then
    echo "Skipping configuration because following file exists:"
    echo $configured_flag
-   echo_eval cd external/$package.git/build
+   echo_eval cd external/$package_top_srcdir.git/build
    echo_eval make -j $n_job install
    echo "bin/get_package.sh: $package OK"
    exit 0
@@ -91,20 +109,45 @@ then
 fi
 echo_eval cd external
 #
-# $package.git
-if [ ! -d "$package.git" ]
+# package top source directory
+if echo $web_page | grep '\.git#' > /dev/null
 then
-   echo_eval git clone $web_page $package.git
-   #
+   if [ ! -d "$package.git" ]
+   then
+      echo_eval git clone $web_page $package_top_srcdir
+   fi
+   echo_eval cd $package_top_srcdir
+   echo_eval git reset --hard
+   echo_eval git checkout --quiet $version
+else
+   if [ "$package" != 'sacado' ]
+   then
+      echo "bin/get_package.sh: $package: expected web_page to end with .git"
+      exit 1
+   fi
+   if [ -e "$version.tar.gz" ]
+   then
+      rm $version.tar.gz
+   fi
+   wget $web_page/$version.tar.gz
+   tar -xzf "$version.tar.gz"
+   mv Trilinos-$version $package_top_srcdir
+   cd $package_top_srcdir
 fi
-echo_eval cd $package.git
-echo_eval git reset --hard
-echo_eval git checkout --quiet $version
 #
 # patch source
 if [ "$package" == 'adolc' ]
 then
    sed -i ADOL-C/src/uni5_for.c -e 's|for (\([ij]\)=|for(int \1=|'
+fi
+if [ "$package" == 'sacado' ]
+then
+   # see https://github.com/trilinos/Trilinos/issues/11923
+   file="packages/teuchos/core/src/Teuchos_BigUIntDecl.hpp"
+   if ! grep '^#include <cstdint>' $file > /dev/null
+   then
+      sed -i $file -e 's|#include <iosfwd>|&\n#include <cstdint>|'
+   fi
 fi
 #
 # build
