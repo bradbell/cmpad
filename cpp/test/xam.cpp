97dbfdb6c72aecac5326e03991294e50c98429b7
Brad Bell <bradbell@seanet.com>
2024-05-27 17:45:33 -0700

    main: xam.cpp: add missing test of llsq_obj.

--- cpp/test/xam.cpp
+++ cpp/test/xam.cpp
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: EPL-2.0 OR GPL-2.0-or-later
 // SPDX-FileCopyrightText: Bradley M. Bell <bradbell@seanet.com>
-// SPDX-FileContributor: 2023 Bradley M. Bell
+// SPDX-FileContributor: 2023-24 Bradley M. Bell
 // ---------------------------------------------------------------------------
 // This test suite runs each of the tests in ../xam/*.cpp
 
@@ -16,17 +16,20 @@
    {  BOOST_CHECK( xam_##name () == true ); } \
    BOOST_AUTO_TEST_SUITE_END()
 
+// BEGIN_SORT_THIS_LINE_PLUS_1
 CMPAD_TEST_EXAMPLE(an_ode)
 CMPAD_TEST_EXAMPLE(csv_read)
 CMPAD_TEST_EXAMPLE(csv_speed)
 CMPAD_TEST_EXAMPLE(csv_write)
 CMPAD_TEST_EXAMPLE(det_by_minor)
 CMPAD_TEST_EXAMPLE(det_of_minor)
-CMPAD_TEST_EXAMPLE(near_equal)
 CMPAD_TEST_EXAMPLE(fun_obj)
 CMPAD_TEST_EXAMPLE(fun_speed)
+CMPAD_TEST_EXAMPLE(llsq_obj)
+CMPAD_TEST_EXAMPLE(near_equal)
 CMPAD_TEST_EXAMPLE(runge_kutta)
 CMPAD_TEST_EXAMPLE(uniform_01)
+// END_SORT_THIS_LINE_MINUS_1
 //
 # if CMPAD_HAS_ADEPT
 CMPAD_TEST_EXAMPLE(gradient_adept)
