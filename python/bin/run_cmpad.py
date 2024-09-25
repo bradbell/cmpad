e7ea79192ff8ab6be5835850067f5b24c7e76180
Brad Bell <bradbell@seanet.com>
2024-09-25 14:43:10 -0700

    pull_11: environment.sh: do not set -e -u in scripts that get sourced.

--- python/bin/run_cmpad.py
+++ python/bin/run_cmpad.py
@@ -36,7 +36,7 @@ import platform
 import numpy
 #
 # cmpad_version
-cmpad_version = 'cmpad-2024.9.23'
+cmpad_version = 'cmpad-2024.9.25'
 # ----------------------------------------------------------------------------
 #
 # program
